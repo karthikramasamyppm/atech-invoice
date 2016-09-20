e31b79d8bfb5c757b6c3d38d45321939edc33b92
lucasmro <lucasmro@gmail.com>
2016-09-20 11:13:11 -0300

    Initial commit

---
+++ src/main/java/br/com/atech/AtechInvoiceApplication.java
@@ -0,0 +1,12 @@
+package br.com.atech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtechInvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtechInvoiceApplication.class, args);
+	}
+}
