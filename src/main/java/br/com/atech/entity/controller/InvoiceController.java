4fc0ae855995c78e89311c65437c3ba90ba4c20d
lucasmro <lucasmro@gmail.com>
2016-09-24 17:37:39 -0300

    Change InvoiceController and add create action

--- src/main/java/br/com/atech/entity/controller/InvoiceController.java
+++ src/main/java/br/com/atech/entity/controller/InvoiceController.java
@@ -1,13 +1,18 @@
 package br.com.atech.entity.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.atech.entity.ApiResponse;
 import br.com.atech.entity.Invoice;
 import br.com.atech.repository.filter.InvoiceSearchFilter;
 import br.com.atech.service.InvoiceService;
@@ -23,7 +28,7 @@ public class InvoiceController {
         this.invoiceService = invoiceService;
     }
 
-    @RequestMapping(method = { RequestMethod.GET })
+    @RequestMapping(method = RequestMethod.GET)
     public Page<Invoice> index( //
             @RequestParam(value = "companyId", required = false) Long companyId, //
             @RequestParam(value = "productId", required = false) Long productId, //
@@ -33,5 +38,12 @@ public class InvoiceController {
         InvoiceSearchFilter filter = new InvoiceSearchFilter(companyId, productId, productName);
 
         return invoiceService.findAllByInvoiceSearchFilter(filter, pageable);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ApiResponse create(@RequestBody @Valid final Invoice invoice) {
+        invoiceService.saveAsync(invoice);
+
+        return new ApiResponse(HttpStatus.ACCEPTED);
     }
 }
